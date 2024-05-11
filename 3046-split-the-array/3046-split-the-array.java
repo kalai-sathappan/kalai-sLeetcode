93ee5294d043f79af9fbec33cb941215d50bea38
Kalaiyarasan S <138379404+kalai-sathappan@users.noreply.github.com>
2024-05-11 19:10:18 +0530

    Time: 2 ms (68.4%), Space: 42.3 MB (91.55%) - LeetHub

---
+++ 3046-split-the-array/3046-split-the-array.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean isPossibleToSplit(int[] nums) {
+        Arrays.sort(nums); 
+
+        int a[] = new int [nums.length/2]; 
+        int b[] = new int [nums.length/2]; 
+
+        int j=0,k=0;
+        for(int i=0;i<nums.length;i=i+2){ 
+            a[j++]=nums[i];
+        } 
+        
+        for(int i=1;i<nums.length;i=i+2){ 
+            b[k++]=nums[i];
+        } 
+
+        
+
+        for(int i=0;i<a.length-1;i++){ 
+            if(a[i]==a[i+1]){ 
+                return false;
+            }
+        } 
+
+        for(int i=0;i<b.length-1;i++){ 
+            if(b[i]==b[i+1]){ 
+                return false;
+            }
+        } 
+
+        return true;
+    }
+}
