2e70548446ef51185dd94a186e547f733b00cd66
Kalaiyarasan S <138379404+kalai-sathappan@users.noreply.github.com>
2024-05-17 21:32:21 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0566-reshape-the-matrix/0566-reshape-the-matrix.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[][] matrixReshape(int[][] mat, int r, int c) {
+        List<Integer> l = new ArrayList<>(); 
+        for(int i = 0;i<mat.length;i++){ 
+            for(int j=0;j<mat[0].length;j++){ 
+                l.add(mat[i][j]);
+            }
+        } 
+
+        int a[][] = new int[r][c]; 
+        int k=0;
+        for(int i=0;i<r;i++){ 
+            for(int j=0;j<c;j++){ 
+                if(k>=l.size()){
+                    return mat;
+                }
+                a[i][j] = l.get(k); 
+                k++;
+            }
+        } 
+        return a;
+    }
+}
