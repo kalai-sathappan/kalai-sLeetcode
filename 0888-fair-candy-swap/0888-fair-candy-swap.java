bf7316b85b64381873462d5150ca90d8cb574a76
Kalaiyarasan S <138379404+kalai-sathappan@users.noreply.github.com>
2024-05-17 18:34:45 +0530

    Time: 1487 ms (5.1%), Space: 46.3 MB (8.22%) - LeetHub

--- 0888-fair-candy-swap/0888-fair-candy-swap.java
+++ 0888-fair-candy-swap/0888-fair-candy-swap.java
@@ -1,39 +1,29 @@
 class Solution {
     public int[] fairCandySwap(int[] aliceSizes, int[] bobSizes) {
-        int a [] = new int[2]; 
+
         int alice =0, bob = 0; 
         for(int i=0;i<aliceSizes.length;i++){ 
             alice += aliceSizes[i];
         } 
 
-        for(int i=0;i<aliceSizes.length;i++){ 
+        for(int i=0;i<bobSizes.length;i++){ 
             bob += bobSizes[i];
         } 
 
         int avg = (alice+bob)/2; 
-        for(int i=0;i<aliceSizes.length;i++){ 
-            for(int j=0;j<bobSizes.length;j++){ 
-                if((aliceSizes[i]+bobSizes[j])==avg){  
-                    a[0]=aliceSizes[i]; 
-                    aliceSizes[i]=0;
-                    break;
-                }
-            } 
-            if(a[0]>0){
-                break;
+
+        List<Integer> l = new ArrayList<>();
+
+        for(int i:bobSizes){ 
+            l.add(i);
+        }           
+
+        for(int i:aliceSizes){ 
+            int target = avg-(alice-i); 
+            if(l.contains(target)){ 
+                return new int[]{i,target};
             }
-        }  
-         for(int i=0;i<bobSizes.length;i++){ 
-            for(int j=0;j<aliceSizes.length;j++){ 
-                if((bobSizes[i]+aliceSizes[j])==avg ){  
-                    a[1]=bobSizes[i]; 
-                    bobSizes[i]=0;
-                    break;
-                }
-            } 
-            if(a[1]>0)
-               break;
-        } 
-        return a;
+        }
+        return new int[]{0,0};
     }
 }
