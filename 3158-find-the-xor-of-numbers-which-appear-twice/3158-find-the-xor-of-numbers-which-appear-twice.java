11414ce2cacb079ee635f1791695638023991f53
Kalaiyarasan S <138379404+kalai-sathappan@users.noreply.github.com>
2024-06-21 23:54:10 +0530

    Time: 3 ms (45.3%), Space: 42.7 MB (79.06%) - LeetHub

--- 3158-find-the-xor-of-numbers-which-appear-twice/3158-find-the-xor-of-numbers-which-appear-twice.java
+++ 3158-find-the-xor-of-numbers-which-appear-twice/3158-find-the-xor-of-numbers-which-appear-twice.java
@@ -1,13 +1,20 @@
 class Solution {
     public int duplicateNumbersXOR(int[] nums) {
         Arrays.sort(nums);
-        int sum = 0;
+        int sum = 0; 
+        List<Integer> l = new ArrayList<>();
         for(int i=1;i<nums.length;i++){ 
             if((nums[i] - nums[i-1])==0){
-                sum += nums[i];
+                l.add(nums[i]);
             }
         } 
-        return sum==0?0:sum;
+        if(l.size()==1){ 
+            return l.get(0);
+        } 
+        for(int i:l){ 
+            sum = sum ^ i;
+        } 
+        return sum;
         
 
     }
