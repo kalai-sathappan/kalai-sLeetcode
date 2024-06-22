19e65b572df96dd29496d07d26fcab52c49bfc2d
Kalaiyarasan S <138379404+kalai-sathappan@users.noreply.github.com>
2024-06-22 11:04:55 +0530

    Time: 5 ms (14%), Space: 43.4 MB (64.94%) - LeetHub

---
+++ 0950-reveal-cards-in-increasing-order/0950-reveal-cards-in-increasing-order.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int[] deckRevealedIncreasing(int[] deck) {
+        ArrayList<Integer> l = new ArrayList<>();
+
+Arrays.sort(deck); // Sort the array
+for (int i = 0; i < deck.length / 2; i++) {
+    int temp = deck[i];
+    deck[i] = deck[deck.length - i - 1];
+    deck[deck.length - i - 1] = temp;
+}
+
+        for(int i=0;i<deck.length;i++) 
+        {  
+            l.add(0,deck[i]); 
+            if(l.size()<=deck.length-1){  
+                int f = l.get(l.size()-1); 
+                l.remove(l.size()-1);
+                l.add(0,f);
+            } 
+           
+        } 
+
+        int a [] = new int[l.size()]; 
+
+        for(int i=0;i<l.size();i++){ 
+            a[i] = l.get(i);
+        }
+        return a;
+    }
+}
