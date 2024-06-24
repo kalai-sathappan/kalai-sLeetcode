255a31492dcf5b473c1ed8b913cf37e7f9d69cb7
Kalaiyarasan S <138379404+kalai-sathappan@users.noreply.github.com>
2024-06-24 17:00:44 +0530

    Time: 3 ms (85.46%), Space: 49.7 MB (29%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -3,14 +3,13 @@ class Solution {
         if (grid == null || grid.length == 0) {
             return 0;
         }
-        
-        boolean[][] visited = new boolean[grid.length][grid[0].length];  
+         
         int count = 0;
         
         for (int i = 0; i < grid.length; i++) { 
             for (int j = 0; j < grid[0].length; j++) { 
-                if (grid[i][j] == '1' && !visited[i][j]) { 
-                    markIsland(i, j, visited, grid); 
+                if (grid[i][j] == '1' ) { 
+                    markIsland(i, j, grid); 
                     count++;
                 }
             }
@@ -19,14 +18,14 @@ class Solution {
         return count;
     } 
     
-    static void markIsland(int i, int j, boolean[][] visited, char[][] grid) { 
-        if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || visited[i][j] || grid[i][j] != '1') {
+    static void markIsland(int i, int j, char[][] grid) { 
+        if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] != '1') {
             return;
         } 
-        visited[i][j] = true; 
-        markIsland(i - 1, j, visited, grid);
-        markIsland(i + 1, j, visited, grid);
-        markIsland(i, j - 1, visited, grid);
-        markIsland(i, j + 1, visited, grid);
+        grid[i][j]='2';
+        markIsland(i - 1, j, grid);
+        markIsland(i + 1, j,  grid);
+        markIsland(i, j - 1,  grid);
+        markIsland(i, j + 1, grid);
     }
 }
