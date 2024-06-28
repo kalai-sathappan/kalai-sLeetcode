2bec6be35318004b96a20369152909d87c2aec63
Kalaiyarasan S <138379404+kalai-sathappan@users.noreply.github.com>
2024-06-28 14:54:26 +0530

    Time: 9 ms (6.41%), Space: 44.6 MB (8.21%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -0,0 +1,60 @@
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        
+        List<String> l = new ArrayList<>(); 
+        par(n,n,"",l); 
+        return l;
+
+    } 
+    static void par(int open,int close,String p,List<String>l){ 
+        if(open == close & open == 0){  
+            if(isValid(p)){ 
+            l.add(p);  
+            return ;  
+            }
+            return;
+        } 
+        if(open > 0){ 
+            par(open-1,close,p+"(",l);
+        } 
+         if(close > 0){ 
+           par(open,close-1,p+")",l);
+        }
+    } 
+     static boolean isValid(String s) {
+        Stack <Character> stack= new Stack<Character>();  
+        for(int i=0;i<s.length();i++){ 
+            if(s.charAt(i)=='(' || s.charAt(i)=='{'|| s.charAt(i)=='['){ 
+                stack.push(s.charAt(i));
+            } 
+            else {  
+                char c=s.charAt(i);
+                if(stack.isEmpty()) 
+                   return false; 
+                if(c==')'){ 
+                    if(stack.peek()=='(')   
+                       stack.pop(); 
+                    else 
+                       return false;
+                } 
+                if(c=='}'){ 
+                    if(stack.peek()=='{')   
+                       stack.pop(); 
+                    else 
+                       return false;
+                } 
+                if(c==']'){ 
+                    if(stack.peek()=='[')   
+                       stack.pop();
+                    else 
+                       return false;
+                }
+               
+            }
+        } 
+        if(stack.isEmpty()){ 
+            return true;
+        } 
+        return false;
+    }
+}
