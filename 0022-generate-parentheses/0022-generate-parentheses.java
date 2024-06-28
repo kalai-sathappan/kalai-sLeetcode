3378bd1c383eedf84dbf4949448f5bb589653aae
Kalaiyarasan S <138379404+kalai-sathappan@users.noreply.github.com>
2024-06-28 14:58:35 +0530

    Time: 3 ms (16.54%), Space: 43 MB (54.48%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -18,27 +18,5 @@ class Solution {
            par(open,close-1,p+")",l);
         }
     } 
-     static boolean isValid(String s) {
-        Stack <Character> stack= new Stack<Character>();  
-        for(int i=0;i<s.length();i++){ 
-            if(s.charAt(i)=='(' ){ 
-                stack.push(s.charAt(i));
-            } 
-            else {  
-                char c=s.charAt(i);
-                if(stack.isEmpty()) 
-                   return false; 
-                if(c==')'){ 
-                    if(stack.peek()=='(')   
-                       stack.pop(); 
-                    else 
-                       return false;
-                } 
-            }
-        } 
-        if(stack.isEmpty()){ 
-            return true;
-        } 
-        return false;
-    }
+     
 }
