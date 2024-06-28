ee01f4f05b81f514b854adebb3be32c5b4d43010
Kalaiyarasan S <138379404+kalai-sathappan@users.noreply.github.com>
2024-06-28 14:58:27 +0530

    Time: 2 ms (54.47%), Space: 43.1 MB (43.5%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,6 +1,6 @@
 class Solution {
-    public List<String> generateParenthesis(int n) {
-        
+    public List<String> generateParenthesis(int n) { 
+
         List<String> l = new ArrayList<>(); 
         par(n,n,"",l); 
         return l;
@@ -8,16 +8,13 @@ class Solution {
     } 
     static void par(int open,int close,String p,List<String>l){ 
         if(open == close & open == 0){  
-            if(isValid(p)){ 
-            l.add(p);  
-            return ;  
-            }
+            l.add(p);
             return;
         } 
         if(open > 0){ 
             par(open-1,close,p+"(",l);
         } 
-         if(close > 0){ 
+         if(close > 0 & close > open){ 
            par(open,close-1,p+")",l);
         }
     } 
