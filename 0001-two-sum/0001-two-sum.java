41544bae883a5a1ae6f3bec1101ddaacc0e160d1
Kalaiyarasan S <138379404+kalai-sathappan@users.noreply.github.com>
2024-07-01 23:02:44 +0530

    Time: 45 ms (29.78%), Space: 44.9 MB (20.92%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,6 +1,6 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int a[]=new int[2]; 
+         int a[]=new int[2]; 
 
         for(int i=0;i<nums.length;i++){ 
             for(int j=i+1;j<nums.length;j++){ 
@@ -12,5 +12,6 @@ class Solution {
             }
         } 
         return a;
+        
     }
 }
